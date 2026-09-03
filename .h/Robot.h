dcb24cbdec34bdeef82a78402f9c647e2b48f877
Luis Samano <luis.samano@lasallesaltillo.edu.mx>
2026-09-03 15:04:23 -0600

    Robot

--- .h/Robot.h
+++ .h/Robot.h
@@ -1,1 +1,29 @@
+#pragma once
+#include <string>
 #include <iostream>
+
+using namespace std;
+
+class Robot{
+private:
+    string nombre;
+    string tipo;
+
+public:
+
+    Robot(string n, string t)
+    {
+        nombre = n;
+        tipo = t;
+    }
+
+    string getNombre()
+    {
+        return nombre;
+    }
+
+    string getTipo()
+    {
+        return tipo;
+    }
+};
