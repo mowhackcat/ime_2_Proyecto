e1549b4128f1a3f474bad7e1f0fe81040a693e14
Yurik <yurikasoz24@gmail.com>
2026-09-09 15:09:36 -0600

    Update Robot.cpp

--- .cpp/Robot.cpp
+++ .cpp/Robot.cpp
@@ -1,1 +1,23 @@
 #include "Robot.h"
+
+Robot::Robot() // <--- Implementación del constructor vacío
+{
+    nombre = "";
+    tipo = "";
+}
+
+Robot::Robot(string n, string t)
+{
+    nombre = n;
+    tipo = t;
+}
+
+string Robot::getNombre()
+{
+    return nombre;
+}
+
+string Robot::getTipo()
+{
+    return tipo;
+}
