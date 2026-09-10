4cc0a600bc037efe02deaf3d4bbffc5c700b055c
mowhackcat <daniel.guerra@lasallesaltillo.edu.mx>
2026-09-09 20:34:38 -0600

    agregar la funcion para elegir al ganador

--- .cpp/Batalla.cpp
+++ .cpp/Batalla.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-#include <Batalla.h>
+#include "Batalla.h"
 #include <cstdlib>
 using namespace std;
 
 Pelea::Pelea(Robot r1,Robot r2){
     Robot robot1 = r1;
     Robot robot2 = r2;
+
+    int resultado = rand ()%2;
+
+    if (resultado==0){
+        ganador = robot1;
+    }else{
+        ganador = robot2;
+    }
 }
